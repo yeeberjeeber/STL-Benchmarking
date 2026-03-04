e1d27ed2e3919cbcf9fab0aba02fd8b4e9df0649
yeeberjeeber <aloysius.tan00@gmail.com>
2026-03-04 22:23:30 +0800

    adding comments, cleaning up

--- STLBenchmarking/benchmark.h
+++ STLBenchmarking/benchmark.h
@@ -3,16 +3,19 @@
 #include <chrono>
 #include <iostream>
 
+using namespace std;
+
+// template is a blueprint to make a function for any type T
 template <typename T>
 void benchmark_vector_push(int N) {
-    std::vector<T> vec;
+    vector<T> vec;
     vec.reserve(N);
 
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = chrono::high_resolution_clock::now();
     for (int i = 0; i < N; ++i)
-        vec.push_back(T{});
-    auto end = std::chrono::high_resolution_clock::now();
+        vec.push_back(T{}); // initializing T to 0, pushing an array of doubles of size 100 into vector
+    auto end = chrono::high_resolution_clock::now();
 
-    std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Vector push time: " << elapsed.count() << " seconds;" << std::endl;
+    chrono::duration<double> elapsed = end - start;
+    cout << "Vector push time: " << elapsed.count() << " seconds;" << std::endl;
 }

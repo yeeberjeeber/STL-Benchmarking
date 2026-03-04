883babcfd4ce68f10619d187ba7634a3f23a25d4
yeeberjeeber <aloysius.tan00@gmail.com>
2026-03-04 20:33:31 +0800

    Add project files.

---
+++ STLBenchmarking/benchmark.h
@@ -0,0 +1,2 @@
+#pragma once
+void benchmark_vector_push(int N);
---
+++ STLBenchmarking/benchmark_vector.cpp
@@ -0,0 +1,22 @@
+#include "benchmark.h"
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+void benchmark_vector_push(int N) {
+	vector<int> vec;
+	vec.reserve(N);
+
+	auto start = chrono::high_resolution_clock::now();
+
+	for (int i = 0; i < N; i++) {
+		vec.push_back(i);
+	}
+	
+	auto end = chrono::high_resolution_clock::now();
+
+	chrono::duration<double> elapsed = end - start;
+	cout << "Vector push time: " << elapsed.count() << " seconds;" << endl;
+}
